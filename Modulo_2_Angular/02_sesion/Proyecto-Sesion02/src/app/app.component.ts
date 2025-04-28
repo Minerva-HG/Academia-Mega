e3955b565edf2df8f99e499a73e32747d9b1a04c
Minerva-HG <61202582+Minerva-HG@users.noreply.github.com>
2025-04-27 19:41:12 -0600

    Sesion 2

---
+++ Modulo_2_Angular/02_sesion/Proyecto-Sesion02/src/app/app.component.ts
@@ -0,0 +1,17 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { Saludo } from './Components/Saludo/saludo.component';
+// import { CardComponent } from './Components/card/card.component';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet, Saludo, CardComponent],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'Mi primera SPA';
+//   author = 'Minerva Hernandez';
+//   perfil = 'QA TESTER';
+//   Expe = "con 7 años de experencia"
+// }
