93ba99b6b92cef7edaf97c15cbc21afa3a1708e5
Minerva-HG <61202582+Minerva-HG@users.noreply.github.com>
2025-05-01 14:54:27 -0600

    Sesion 6

---
+++ Modulo_2_Angular/06_sesion/Proyecto-06/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { RouterEvent, RouterLink, RouterOutlet } from '@angular/router';
+import { CardComponent } from "./components/card/card.component";
+import { TarjetaComponent } from "./components/tarjeta/tarjeta.component";
+import { CommonModule } from '@angular/common';
+import { TodoComponent } from "./components/todo/todo.component";
+import { OtroComponent } from "./components/Otro/Otro.component";
+import { ProductManagerComponent } from "./components/product-manager/product-manager.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CardComponent, TarjetaComponent, CommonModule, TodoComponent, ProductManagerComponent, RouterLink, OtroComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ejemplo3';
+
+  isVisible = true;
+  frutas = ["Manzana", "Platano", "Naranja", "Uva", "Piña"];
+
+  ///////////////////////
+  rolUsuario = "admin";
+  edad = 3;
+  tareaImportante = true;
+  isUrgente = true;
+
+}
