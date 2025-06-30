39a15a42a9426320a0af60459dde26ee54cf2877
Minerva-HG <minerva.hernandez@megacable.com.mx>
2025-06-29 22:37:53 -0600

    Sesion 5 font and back 27-06-2025

--- APPNODE/DemoApp/obj/Debug/net8.0/DemoApp.AssemblyInfo.cs
+++ APPNODE/DemoApp/obj/Debug/net8.0/DemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97c2c0333ec2b2ef3323830d1de3c6591943cd5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa51c35d3418953da17e26c92f4fc180033e36f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
