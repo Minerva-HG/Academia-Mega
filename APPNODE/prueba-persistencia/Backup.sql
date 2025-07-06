77eed63f77b84c25b79dc62472d5bef0f68a824b
Minerva-HG <minerva.hernandez@megacable.com.mx>
2025-07-05 22:06:36 -0600

    sesion 10

---
+++ APPNODE/prueba-persistencia/Backup.sql
@@ -0,0 +1,21 @@
+ALTER TABLE nombre_tabla
+ADD COLUMN nueva_columna tipo_dato;             
+
+
+ALTER TABLE nombre_tabla
+ADD CONSTRAINT nombre_restriccion
+CHECK (condicion);
+
+
+
+
+
+ALTER TABLE nombre_tabla
+ADD CONSTRAINT nombre_restriccion           
+
+
+
+CHECK (condicion);  
+
+
+ALTER TABLE nombre_tabla
